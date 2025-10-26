635a40836b9c980d14dc3e684d54c8589f916ab5
Olav <olav@olavorw.com>
2025-10-26 11:25:37 -0700

    util fix

--- utils/zsh.sh
+++ utils/zsh.sh
@@ -4,29 +4,14 @@
 
 set -e
 
-ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+ZSH_CUSTOM="${ZSH_CUSTOM:-/.oh-my-zsh/custom}"
 PLUGIN_DIR="$HOME/.zsh"
 
 # Create plugin directory
 mkdir -p "$PLUGIN_DIR"
 
-# Clone zsh-autosuggestions
-if [ ! -d "$PLUGIN_DIR/zsh-autosuggestions" ]; then
-  echo "Cloning zsh-autosuggestions..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "$PLUGIN_DIR/zsh-autosuggestions"
-else
-  echo "zsh-autosuggestions already exists"
-fi
-
-# Clone zsh-syntax-highlighting
-if [ ! -d "$PLUGIN_DIR/zsh-syntax-highlighting" ]; then
-  echo "Cloning zsh-syntax-highlighting..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting \
-    "$PLUGIN_DIR/zsh-syntax-highlighting"
-else
-  echo "zsh-syntax-highlighting already exists"
-fi
+git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone "https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 echo "Plugins cloned to $PLUGIN_DIR"
 echo "Add to your .zshrc:"
